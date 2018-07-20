1219bd012006ccd323f3f1ef7996c0dd42f44628
jlai <result0924@gmail.com>
2018-07-20 13:00:53 +0800

    [#1] Add simple sample to save and load password

--- keyChainTest/keyChainTest/ViewController.swift
+++ keyChainTest/keyChainTest/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let service = "myService"
+    let account = "myAccount"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        KeychainService.savePassword(service: service, account: account, data: "7533967")
+        
+        if let password = KeychainService.loadPassword(service: service, account: account) {
+            print(password)
+        }
     }
 
     override func didReceiveMemoryWarning() {
