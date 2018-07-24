c11f30431d040c98a5e9b114a60b4dac39bc5498
jlai <result0924@gmail.com>
2018-07-24 17:50:45 +0800

    [#1] Add new target `keyChainTest2`

---
+++ keyChainTest/keyChainTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keyChainTest2
+//
+//  Created by justin on 2018/7/24.
+//  Copyright © 2018 justin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
