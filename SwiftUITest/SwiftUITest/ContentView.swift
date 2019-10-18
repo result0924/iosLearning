3156f79dd1b09de7b531051eff75c009c2c7b581
jlai <result0924@gmail.com>
2019-10-18 17:18:27 +0800

    Add SwiftUITest project

---
+++ SwiftUITest/SwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by justin on 2019/10/18.
+//  Copyright © 2019 jlai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
