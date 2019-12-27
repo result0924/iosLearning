1256146bd3ae22c84494150b1ef5a24071b88d40
jlai <result0924@gmail.com>
2019-12-27 18:30:46 +0800

    Add Coordinator test project

---
+++ CoordinatorTest/CoordinatorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by justin on 2019/12/27.
+//  Copyright © 2019 jlai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
