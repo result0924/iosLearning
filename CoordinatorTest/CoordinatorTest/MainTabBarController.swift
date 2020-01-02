e2c7b4909a77941b4a2797fd0852405b9362b6d3
jlai <result0924@gmail.com>
2020-01-02 11:42:39 +0800

    Add simple tabbar scenario

---
+++ CoordinatorTest/CoordinatorTest/MainTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabBarController.swift
+//  CoordinatorTest
+//
+//  Created by justin on 2020/01/02.
+//  Copyright © 2020 jlai. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- CoordinatorTest/CoordinatorTest/ViewController.swift
+++ CoordinatorTest/CoordinatorTest/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
