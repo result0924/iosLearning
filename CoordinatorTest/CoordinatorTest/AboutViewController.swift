4071554a33e1821d779b570b0ab9d031b9f61a3c
jlai <result0924@gmail.com>
2020-01-03 18:39:51 +0800

    Add AboutViewController

---
+++ CoordinatorTest/CoordinatorTest/AboutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutViewController.swift
+//  CoordinatorTest
+//
+//  Created by justin on 2020/01/03.
+//  Copyright © 2020 jlai. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
