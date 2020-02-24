ddb425a9c84f5f6add243fff89b229e1aa498477
jlai <result0924@gmail.com>
2020-02-24 11:09:09 +0800

    Add SignInWithAppleTest project

---
+++ SignInWithAppleTest/SignInWithAppleTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignInWithAppleTest
+//
+//  Created by justin on 2020/02/24.
+//  Copyright © 2020 jlai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
