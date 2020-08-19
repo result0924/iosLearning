cb7dfa094a0f8ffa173d700f26d72867e47d9202
jlai <result0924@gmail.com>
2020-08-19 15:35:38 +0800

    Add Firebase carthage test project

---
+++ FirebaseCarthageTest/FirebaseCarthageTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseCarthageTest
+//
+//  Created by jlai on 2020/8/19.
+//  Copyright © 2020 jlai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
