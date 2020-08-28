f4d5cf2caf1d6247f1a807d0a31cf274f9ca448c
jlai <result0924@gmail.com>
2020-08-28 16:29:44 +0800

    Add empty webview to teset

---
+++ WebViewTest/WebViewTest/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by jlai on 2020/8/28.
+//  Copyright © 2020 jlai. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        clearCache()
+        
+        loadURL(urlString: "https://10.0.1.86:8080/webview")
+    }
+    
+    private func loadURL(urlString: String) {
+        let url = URL(string: urlString)
+        if let url = url {
+            let request = URLRequest(url: url)
+            webView.navigationDelegate = self
+            webView.load(request)
+        }
+    }
+    
+    func clearCache() {
+        if #available(iOS 9.0, *) {
+            let websiteDataTypes = NSSet(array: [WKWebsiteDataTypeDiskCache, WKWebsiteDataTypeMemoryCache])
+            let date = NSDate(timeIntervalSince1970: 0)
+            WKWebsiteDataStore.default().removeData(ofTypes: websiteDataTypes as! Set<String>, modifiedSince: date as Date, completionHandler:{ })
+        } else {
+            var libraryPath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.libraryDirectory, FileManager.SearchPathDomainMask.userDomainMask, false).first!
+            libraryPath += "/Cookies"
+
+            do {
+                try FileManager.default.removeItem(atPath: libraryPath)
+            } catch {
+                print("error")
+            }
+            URLCache.shared.removeAllCachedResponses()
+        }
+    }
+
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Strat to load")
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("finish to load")
+    }
+    
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+            if challenge.previousFailureCount == 0 {
+                let credential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+                completionHandler(URLSession.AuthChallengeDisposition.useCredential,credential)
+            }else{
+                completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge,nil)
+            }
+        }else{
+            completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge,nil)
+        }
+    }
+}
+
+extension WKWebView {
+    class func clean() {
+        guard #available(iOS 9.0, *) else {return}
+
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+                #if DEBUG
+                    print("WKWebsiteDataStore record deleted:", record)
+                #endif
+            }
+        }
+    }
+}
