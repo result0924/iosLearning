22bb04ebbd4ea72b2493e703ea467fc72628c475
jlai <result0924@gmail.com>
2021-07-28 17:46:47 +0800

    Add VerifyTextFieldTest project

---
+++ VerifyTextFieldTest/VerifyTextFieldTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VerifyTextFieldTest
+//
+//  Created by lai Kuan-Ting on 2021/7/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
