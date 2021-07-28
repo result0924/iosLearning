3f1a9c309eacd30df186738e79bf2c54a143a164
jlai <result0924@gmail.com>
2021-07-28 18:19:21 +0800

    Add CustomTextView folder/swift/xib

---
+++ VerifyTextFieldTest/VerifyTextFieldTest/CustomTextView/CustomTextView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextView.swift
+//  VerifyTextFieldTest
+//
+//  Created by lai Kuan-Ting on 2021/7/28.
+//
+
+import UIKit
+
+class CustomTextView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
