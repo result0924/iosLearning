9569f04e804e4103f952c187e3f682b9a8fc6d3a
jlai <result0924@gmail.com>
2021-07-29 15:39:30 +0800

    Add custom text view example

--- VerifyTextFieldTest/VerifyTextFieldTest/CustomTextView/CustomTextView.swift
+++ VerifyTextFieldTest/VerifyTextFieldTest/CustomTextView/CustomTextView.swift
@@ -7,14 +7,81 @@
 
 import UIKit
 
-class CustomTextView: UIView {
+protocol CustomTextViewDelegate: AnyObject {
+    func textIsValid(textFiledTag: Int, isValid: Bool)
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+@IBDesignable
+class CustomTextView: UIView, NibOwnerLoadable {
+
+    @IBInspectable var placeholder: String = "" {
+        didSet {
+            textField.placeholder = placeholder
+        }
     }
-    */
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        textField.placeholder = placeholder
+    }
+    
+    // MARK: IBOutlet
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var warningLabel: UILabel!
+    @IBOutlet private weak var bottomLineView: UIView!
+    private var regexString: String?
+    private var warningString = ""
+    weak var delegate: CustomTextViewDelegate?
+    
+    // MARK: Initial
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    // MARK: Custom Init
+    private func customInit() {
+        loadNibContent()
+        textField.delegate = self
+    }
+    
+    // MARK: Awake from nib
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        textField.placeholder = placeholder
+    }
+    
+    func updateCheckInfo(tag: Int, regex: String, warning: String) {
+        regexString = regex
+        warningString = warning
+        textField.tag = tag
+    }
 
+}
+
+extension CustomTextView: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text,
+           let regexString = regexString {
+            let tempString = (text as NSString).replacingCharacters(in: range, with: string)
+            let isValid = tempString.isValid(regularExpression: regexString)
+            warningLabel.text = isValid || tempString.isEmpty ? "" : warningString
+            delegate?.textIsValid(textFiledTag: textField.tag, isValid: isValid)
+        }
+        
+        return true
+    }
+    
+}
+
+extension String {
+    func isValid(regularExpression: String) -> Bool {
+        return NSPredicate(format: "SELF MATCHES %@", regularExpression).evaluate(with: self)
+    }
 }
--- VerifyTextFieldTest/VerifyTextFieldTest/ViewController.swift
+++ VerifyTextFieldTest/VerifyTextFieldTest/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    enum textViewType: Int {
+        case firstName
+        case lastName
+    }
 
+    struct checkItem {
+        let item: textViewType
+        var isValid: Bool
+    }
+    
+    @IBOutlet private weak var lastNameView: CustomTextView!
+    @IBOutlet private weak var firstNameView: CustomTextView!
+    @IBOutlet private weak var completeButton: UIButton!
+    private var checkItemArray: [checkItem] = [.init(item: .firstName, isValid: false), .init(item: .lastName, isValid: false)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let regexString = "[a-z]{0,6}"
+        let warningString = "word must be english and can't more than six"
+        lastNameView.updateCheckInfo(tag: textViewType.lastName.rawValue, regex: regexString, warning: warningString)
+        lastNameView.delegate = self
+        firstNameView.updateCheckInfo(tag: textViewType.firstName.rawValue, regex: regexString, warning: warningString)
+        firstNameView.delegate = self
     }
 
-
+    func checkItemStatus(textFiledTag: Int, isValid: Bool) {
+        checkItemArray.indices.forEach {
+            if checkItemArray[$0].item.rawValue == textFiledTag {
+                checkItemArray[$0].isValid = isValid
+            }
+        }
+        
+        completeButton.isEnabled = checkItemArray.filter({$0.isValid == true}).count == checkItemArray.count
+        
+    }
+    
+    @IBAction func tapCompleteButton(_ sender: Any) {
+        print("tapCompleteButton")
+    }
 }
 
+extension ViewController: CustomTextViewDelegate {
+    func textIsValid(textFiledTag: Int, isValid: Bool) {
+        checkItemStatus(textFiledTag: textFiledTag, isValid: isValid)
+    }
+}
