8288f92ca32cac86e0d63bfbe74c10332d828553
jlai <result0924@gmail.com>
2021-09-14 11:07:43 +0800

    Add TrackCellularDataUsageTest project

---
+++ TrackCellularDataUsageTest/TrackCellularDataUsageTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackCellularDataUsageTest
+//
+//  Created by lai Kuan-Ting on 2021/9/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
