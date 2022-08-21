76f70a1e51c7c23d8e703ba78c432887aefab456
jlai <result0924@gmail.com>
2022-08-21 22:56:11 +0800

    Update Asynchronoush playground

--- playgroundTest/Asynchronous.playground/Pages/Asynchronous2.xcplaygroundpage/Contents.swift
+++ playgroundTest/Asynchronous.playground/Pages/Asynchronous2.xcplaygroundpage/Contents.swift
@@ -9,13 +9,11 @@ let totalSelfMonitor = 5
 var finishSelfMonitor = 0
 
 func selfMonitorADay(name: String) {
-    print(Thread.current)
     print("\(name): 1️⃣ 起床")
     sleep(1)
     print("\(name): 2️⃣ 開始量血糖")
     sleep(1)
     Task {
-        print(Thread.current)
         print("\(name): 3️⃣ 開始量飯前血糖")
         sleep(1)
         print("\(name): 4️⃣ 吃晚餐")
@@ -36,6 +34,4 @@ for patient in 1...totalSelfMonitor {
        selfMonitorADay(name: "病患 \(patient) ")
     }
 }
-
-PlaygroundPage.current.needsIndefiniteExecution = true // begin async execution
 //: [Next](@next)
