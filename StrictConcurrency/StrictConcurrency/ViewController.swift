c2afb3b66ebef3a35ebd6f9de69e685553940778
jlai <822363+result0924@users.noreply.github.com>
2025-12-18 16:43:51 +0800

    Initialize StrictConcurrency project with essential files including AppDelegate, SceneDelegate, ViewController, and UI test setup.

---
+++ StrictConcurrency/StrictConcurrency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrictConcurrency
+//
+//  Created by Justin Lai on 2025/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
